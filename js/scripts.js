c95358bfe27337e37179590bf24facd2f2e893ad
zachmeicho <zmeicho19@gmail.com>
2019-05-18 20:45:17 -0700

    Basic site outline

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ console.log('OK');
 
 const player = new Plyr( '#player' , {
   
-    controls : ['play' , 'progress' , 'fullscreen'],
-    muted : true,
-    duration : 20,
+    controls : ['play' , 'progress' , 'fullscreen' , 'volume' , 'mute'],
+    muted : true, 
+    autoplay : true
     
     
   } );
