6680b20fa70be7dd3e8ecb4f6ca5f3b38bbd3141
zachmeicho <zmeicho19@gmail.com>
2019-05-19 19:05:40 -0700

    Added lettering.js

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,7 @@ const player = new Plyr( '#player' , {
     
   } );
 
-//broken below
-  $('item1').hover( function(){
-  
-    $(divdivdivp).addClass('tr');
-    console.log('OK');
-    
-    
-  } );
+
+//Lettering.js
+$('h1').lettering('lines');
+
